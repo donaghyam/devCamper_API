904113f4b7f5089d87ac33aa92ff2b682e4b6e2e
Donaghy <adam.donaghy@capgemini.com>
2023-03-15 10:24:37 -0500

    added controller and reformatted

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,36 +1,20 @@
-// create route
-// handle get request to index (/)
-// arrow function - take in request and response objects
-app.get('/api/v1/bootcamps', (req, res) => {
-  // res.send('Hello from express');
+const express = require('express');
+const {
+  getBootcamps,
+  getSingleBootcamp,
+  createBootcamp,
+  updateBootcamp,
+  deleteBootcamp,
+} = require('../controllers/bootcamps');
 
-  // res.send({ name: 'Adam'})  // this will send the object in JSON format, but the code below is preferred
-  // res.json({ name: 'Adam' });
+const router = express.Router();
 
-  // res.sendStatus(400); // this will show 'Bad Request' in response body
+router.route('/').get(getBootcamps).post(createBootcamp);
 
-  // this is a better response, as it provides the 400 error in addition to the JSON object
-  // res.status(200).json({ success: true, data: { id: 1 } });
+router
+  .route('/:id')
+  .get(getSingleBootcamp)
+  .put(updateBootcamp)
+  .delete(deleteBootcamp);
 
-  res.status(200).json({ success: true, msg: 'Show all bootcamps' });
-});
-
-app.get('/api/v1/bootcamps/:id', (req, res) => {
-  res.status(200).json({ success: true, msg: `Get bootcamp ${req.params.id}` });
-});
-
-app.post('/api/v1/bootcamps', (req, res) => {
-  res.status(200).json({ success: true, msg: 'Create new bootcamp' });
-});
-
-app.put('/api/v1/bootcamps/:id', (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Update bootcamp ${req.params.id}` });
-});
-
-app.delete('/api/v1/bootcamps/:id', (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
-});
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+// Route files
+const bootcamps = require('./routes/bootcamps');
+
 // Load environemnt variables
 // call config on dotenv object
 // add path to location
@@ -8,6 +11,11 @@ dotenv.config({ path: './config/config.env' });
 
 // initialize app variable with express
 const app = express();
+
+// Mount routers
+// connect to bootcamps file
+app.use('/api/v1/bootcamps', bootcamps);
+// we no longer need '/api/v1/bootcamps' in the bootcamps file
 
 // initialize PORT variable
 // set it to what is in the environment variables
